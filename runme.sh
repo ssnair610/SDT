3b0b8d3eb651d5755ba55e2a428a296c04fc23fd
Vivek Radhakrishnan <55615242+SassyQuatch47@users.noreply.github.com>
2022-12-17 09:32:53 +0530

    Update runme.sh
    Added server setup prompt

--- runme.sh
+++ runme.sh
@@ -1,4 +1,16 @@
 # !/bin/bash
+# !/bin/bash
 pip install argparse
 pip install tqdm
 pip install npm
+
+read -p "initiate server setup?(Y/N):" setup_mode
+
+if [ $setup_mode -eq "Y" ] then
+    if [ $# -eq 1 ] then
+        ./server-side/calibrate.py $1
+    elif [ $# -gt 1 ] then
+        ./server-side/calibrate.py $1 -p $2
+    else
+        echo "Setup incomplete. Run `python -u server-side/calibrate.py with network adapter name to calibrate server connection."
+fi
