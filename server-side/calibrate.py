572a793a51f4dab2338760ca9b3c001e177233fb
Vivek Radhakrishnan <55615242+SassyQuatch47@users.noreply.github.com>
2022-12-17 09:25:58 +0530

    Update calibrate.py
    Now contains TextTag Implementation and fixed path resolution for config.json

--- server-side/calibrate.py
+++ server-side/calibrate.py
@@ -1,7 +1,16 @@
+"""Run me to calibrate server configurations with respect to network adapter provided on command line.
+"""
+
 import subprocess
 import argparse
 import json
+import sys
+import os
 import re
+
+__home__ = os.path.abspath(__file__)
+sys.path.append(os.path.dirname(os.path.dirname(__home__)))
+from mytoolkit.txttag import TextTag as tag
 
 if __name__ == "__main__":
     # import argparse
@@ -15,23 +24,24 @@ if __name__ == "__main__":
     network = args.network
     port = args.port
 
-    proc = subprocess.check_output("ipconfig" ).decode('utf-8')
-    findings = re.findall(r'([\w\-\* ]+):\r\s.*\s.*\s+IPv4 Address. . . . . . . . . . . : ([0-9.]*)', proc)
+    proc = subprocess.check_output("ipconfig").decode('utf-8')
+    findings = re.findall(r'([\w\-\* ]*\w):[\w\-\*\:\.\s]+?IPv4 Address. . . . . . . . . . . : ([\d\.]*)', proc)
 
     IPMap = dict()
 
     for finding in findings:
         IPMap[finding[0]] = finding[1]
 
-    config_file = open("config.json", "r")
+    config_file = open(__home__ + "config.json", "r")
     
     configuration = json.load(config_file)
+
     # configuration['serverIP'] = IPMap['Wireless LAN adapter Wi-Fi']
     try:
         configuration['serverIP'] = IPMap[network]
     
     except KeyError:
-        print('Error: Adapter not recognized')
+        print(f'{tag.error.b()}ERROR:{tag.error} Adapter not recognized{tag.close}')
         exit(1)
 
     if port is not None:
@@ -39,9 +49,9 @@ if __name__ == "__main__":
     
         config_file.close()
     
-    config_file = open("config.json", "w")
+    config_file = open(__home__ + "config.json", "w")
     
     json.dump(configuration, config_file, indent=4)
     
     config_file.close()
-    print(f"Setup complete. Server configured with Network[{network}]")
+    print(f"{tag.info.b()}Setup complete.{tag.info} Server configured with network {tag.id}{network}{tag.close}")
