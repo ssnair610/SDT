25657cddb448e3d6f78f74e1322bdbd4fbbd5e48
Vivek Radhakrishnan <55615242+SassyQuatch47@users.noreply.github.com>
2022-12-19 21:03:34 +0000

    Deleting everything for pythonic merge

--- runme.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-pip install argparse
-pip install tqdm
-pip install npm
-npm install -g @angular/cli
-npm install
-npm audit fix --force
--- typings/index.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference path="modules/crypto-js/index.d.ts" />
