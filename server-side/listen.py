32a763f12a1c65c6debbca36e7bfbc31a046a908
Vivek Radhakrishnan <55615242+SassyQuatch47@users.noreply.github.com>
2022-12-20 01:30:26 +0530

    Added server-side/listen.py
    Listener for incoming packets

---
+++ server-side/listen.py
@@ -0,0 +1,71 @@
+"""wait for client response
+"""
+
+import socket
+import shutil
+import json
+import tqdm
+import sys
+import os
+
+__home__ = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(__home__))
+
+from mytoolkit.txttag import TextTag as tag
+
+config_file = open(f'{__home__}/config.json', 'r')
+configuration = json.load(config_file)
+
+SERVER_HOST = configuration['serverIP']
+SERVER_PORT = configuration['vanilla port']
+
+config_file.close()
+
+BUFFER_SIZE = 1024 * 4
+SEPARATOR = "<SEPARATOR>"
+
+socket_ref = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
+socket_ref.bind((SERVER_HOST, SERVER_PORT))
+
+socket_ref.listen(5)
+print(f"{tag.info.b()}[*]{tag.info} Listening as {tag.id}{SERVER_HOST}{tag.white}:{tag.id}{SERVER_PORT}{tag.close}")
+
+client_socket, address = socket_ref.accept()
+print(f"{tag.info.b()}[+] {tag.id}{address}{tag.info} is connected.{tag.close}")
+
+received = client_socket.recv(BUFFER_SIZE).decode()
+filename, filesize = received.split(SEPARATOR)
+filename = os.path.basename(filename)
+filesize = int(filesize)
+
+progress = tqdm.tqdm(
+    range(filesize),
+    f"Receiving {tag.id}{filename}{tag.close}",
+    unit="B", unit_scale=True, unit_divisor=1024
+    )
+
+inbound_file_addr = f'{__home__}/inbound/{filename}'
+
+with open(inbound_file_addr, "wb") as file_b:
+    while True:
+        bytes_read = client_socket.recv(BUFFER_SIZE)
+
+        if not bytes_read: # Received empty Data
+            break # Proceed to connection closure
+
+        file_b.write(bytes_read)
+        progress.update(len(bytes_read))
+
+    file_b.flush() # Prompt I/O Buffer to write immediately
+
+
+client_socket.close()
+socket_ref.close()
+
+targetDir = os.path.splitext(inbound_file_addr)[0]
+shutil.unpack_archive(inbound_file_addr, targetDir, 'zip')
+
+try:
+    os.remove(inbound_file_addr)
+except OSError as e:
+    print (f"\r\n{tag.error.b()}ERROR:{tag.error}{e.strerror}{tag.close}")
