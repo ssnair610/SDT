ce82a2724d6dbb9abc312bcc5eb446ffa225b9de
Vivek Radhakrishnan <55615242+SassyQuatch47@users.noreply.github.com>
2022-12-20 02:22:08 +0530

    Update encrypting.py
    Fixed some bugs and added additional exception handling

--- mytoolkit/core/encrypting.py
+++ mytoolkit/core/encrypting.py
@@ -11,23 +11,28 @@ import core.custom_encrypt as custom_enc
 class des:
     @staticmethod
     def encryption(dataFile, key):
-        des_instance = custom_enc.des(key.export_key(format='DER')[:8])
-
-        print(f"{tag.info.b()}[*]{tag.info} Encrypting: {tag.id}{dataFile.name[:-15]}...{tag.close}")
-
         try:
-            dataFile.seek(0)
-            ciphertext = des_instance.encrypt(dataFile.read(), padmode=custom_enc.PAD_PKCS5)
+            des_instance = custom_enc.des(key[:8])
 
-            return ciphertext
-        
+            print(f"{tag.info.b()}[*]{tag.info} Encrypting: {tag.id}{dataFile.name[:-15]}...{tag.close}")
+
+            try:
+                dataFile.seek(0)
+                ciphertext = des_instance.encrypt(dataFile.read(), padmode=custom_enc.PAD_PKCS5)
+
+                return ciphertext
+            
+            except Exception as e:
+                print(f"{tag.error.b()}[-] ERROR:{tag.error} Encryption failed.{tag.close}\r\n{e}")
+                raise e
+            
         except Exception as e:
-            print(f"{tag.error.b()}[-] ERROR:{tag.error} Encryption failed.{tag.close}\r\n{e}")
+            print(f"{tag.error.b()}[-] ERROR:{tag.error} DES instance failed to initialize.{tag.close}\r\n{e}")
             raise e
         
     @staticmethod
     def encryptionstr(data:str, key):
-        des_instance = custom_enc.des(key.export_key(format='DER')[:8])
+        des_instance = custom_enc.des(key[:8])
 
         print(f"{tag.info.b()}[*]{tag.info} Encrypting: {tag.id}{data[:-15]}...{tag.close}")
 
@@ -44,7 +49,7 @@ class des:
     @staticmethod
     def decryption(dataFile, key):
         try:
-            des_instance = custom_enc.des(key.export_key(format='DER')[:8])
+            des_instance = custom_enc.des(key[:8])
             
             print(f"{tag.info.b()}[*]{tag.info} Decrypting: {tag.id}{dataFile.name[:-15]}...{tag.close}")
         
@@ -70,7 +75,7 @@ class des:
     @staticmethod
     def decryptionstr(data, key):
         try:
-            des_instance = custom_enc.des(key.export_key(format='DER')[:8])
+            des_instance = custom_enc.des(key[:8])
             # data_b = data.encode().hex()
             # print(f"{tag.info.b()}[*]{tag.info} Decrypting: {tag.id}{data_b}{tag.close}")
             # print(f"{tag.info.b()}[@]{tag.info} Key {tag.id}{key.export_key(format='DER')[:8]}{tag.close}")
