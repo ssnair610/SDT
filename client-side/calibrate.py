21376131035d5f440a12590fbbc299a27df63f7c
Vivek Radhakrishnan <55615242+SassyQuatch47@users.noreply.github.com>
2022-12-24 17:28:30 +0530

    Update calibrate.py
    Added key checking

--- client-side/calibrate.py
+++ client-side/calibrate.py
@@ -1,92 +1,147 @@
-"""Run me to calibrate client configurations with respect to network adapter provided on command line.
+"""invasively contact server
 """
 
-import subprocess
-import argparse
+import socket
+import shutil
+import tqdm
 import json
 import sys
 import os
-import re
 
 __home__ = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(__home__))
 
+from mytoolkit.core.keymaker import generateAtDir as make_key_at
 from mytoolkit.txttag import TextTag as tag
+from mytoolkit.core.brain import fileThinker as thinker
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 1024 * 4
 
-    # Catch network name and port address from command line
-    parser.add_argument("network", help="Network Adaptor Name")
-    parser.add_argument("-p", "--port", help=f"Port to use, default is {None}", default=None)
+def check_keychain() -> None:
+    """
+    """
+    if not os.path.isfile(f'{__home__}/metadata/privkey.pem'):
+        try:
+            make_key_at(f'{__home__}/metadata/', asym=True)
+        
+        except Exception as e:
+            raise e
 
-    
-    # Gather network name and port address from parser
-    args = parser.parse_args()
-
-    network = args.network
-    port = args.port
-
-
-    # Windows specific code: Catch Network names and their IPV4 addresses
-    ipconfig_results = subprocess.check_output("ipconfig").decode('utf-8') 
-    findings = re.findall(r'([\w\-\* ]*\w):[\w\-\*\%\=\<\>\[\]\:\.\s]+?IPv4 Address. . . . . . . . . . . : (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', ipconfig_results)
-
-
-    # Map Network names to their IPV4 addresses
-    IPMap = { finding[0] : finding[1] for finding in findings }
+    if not os.path.isfile(f'{__home__}/metadata/key.key'):
+        try:
+            make_key_at(f'{__home__}/metadata/', asym=False)
+        
+        except Exception as e:
+            raise e
 
 
+def fetch_config(config_filename:str=f'{__home__}/metadata/config.json') -> dict:
+    """
+    Loads .json configurations onto a dictionary and returns it
+    """
+    config_file = open(config_filename)
+    configuration = json.load(config_file)
+    config_file.close()
+
+    return configuration
+
+def pack_package() -> str:
+    """
+    Zips and encrypts file based on algorithm contract.json
+    """
+    # Zip the container directory and store it in the parent directory
+    compr_file = shutil.make_archive(f"{__home__}/outbound/container", 'zip', f'{__home__}/outbound/container')
+    files_to_kill = os.listdir(f'{__home__}/outbound/container')
+
+    for file in files_to_kill:
+        os.remove(f'{__home__}/outbound/container/{file}')
+
+    # Load in algorithm contract to specify operations
+    alg_contract_file = open(f'{__home__}/outbound/algorithm contract.json')
+    alg_contract:dict[str, str] = json.load(alg_contract_file)
+
+    # Prepare processed file name
+    encr_file = compr_file + '.encrypted'
+
+    # Using contract process file; File is taken from metadata directory
+    # Output is written into encr_file, Input from compr_file
+    myThinker = thinker(sourceFile = compr_file, targetFile = encr_file, keyAddress=f'{__home__}/metadata/')
+    myThinker.think(contract=alg_contract)
+    myThinker.close()
+
+    # Delete original unprocessed file
+    os.remove(compr_file)
+
+    # Return encrypted file address
+    return encr_file
+
+def pack_and_ship(host:str, port:str) -> None:
+    """
+    """
     try:
-        # Prepare to set client configurations
-        configuration = dict()
+        # Pack and get package address (encrypted compressed file)
+        package = pack_package()
+        filesize = os.path.getsize(package)
 
         try:
-            # Setting serverIP to requested network
-            configuration['serverIP'] = IPMap[network]
-            print(f'{tag.info}Found {tag.id}{network}{tag.info.b()} network IP Address.{tag.info} Processing...{tag.close}')
-        
-        except KeyError as e:
-            print(f'{tag.error.b()}ERROR:{tag.error} Adapter not recognized:{tag.id}{e}{tag.close}')
-            exit(1)
-
-        
-        # If port is specified, assign port address, else retain same port address as in configuration file
-        if port is None:
-            # Open configuration file to write into
-            try:
-                config_file = open(f"{__home__}/metadata/config.json", "r")
-                
-                configuration['vanilla port'] = json.load(config_file)['vanilla port']
-                config_file.close()
-
-            except KeyError as e:
-                print(f'{tag.error.b()}ERROR:{tag.error} configuration file field missing:{tag.id}{e}{tag.close}')
-                exit(1)
+            # Initiate socket
+            socket_ref = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
+            print(f"{tag.info.b()}[*]{tag.info} Connecting to {tag.id}{host}{tag.white}:{tag.id}{port}{tag.close}")
             
-            except json.JSONDecodeError as e:
-                print(f'{tag.error.b()}ERROR:{tag.error} json file decode failed:{tag.id}{e}{tag.close}')
-                exit(1)
+            # Establish connection to server
+            socket_ref.connect((host, port))
+            print(f"{tag.info.b()}[+]{tag.info} Connected.{tag.close}")
 
-            except FileNotFoundError as e:
-                print(f'{tag.error.b()}ERROR:{tag.error} configuration file not found:{tag.id}{e}{tag.close}')
-                exit(1)
+            # Send initial 'heads up' message
+            socket_ref.send(f"{package}{SEPARATOR}{filesize}".encode())
+            progress = tqdm.tqdm(
+                range(filesize),
+                f"Sending {tag.id}{package}{tag.close}",
+                unit="B", unit_scale=True, unit_divisor=1024)
 
-        else:
-            configuration['vanilla port'] = int(port)
+            # Open package and transmit data with BUFFER_SIZE as packet size
+            with open(package, "rb") as file_b:
+                while True:
+                    bytes_read = file_b.read(BUFFER_SIZE)
 
-        # Overwrite configuration in configuration file
-        config_file = open(f'{__home__}/outbound/container/metadata.json', 'w')
+                    if not bytes_read: # Sending empty (close connection)
+                        break # Proceed to connection closure
+
+                    socket_ref.sendall(bytes_read)
+                    progress.update(len(bytes_read))
+                file_b.flush()
+
+            # Terminate transmission connection
+            
+            socket_ref.close()
+            print(f"\n\r{tag.info.b()}[+]{tag.info} Connection to {tag.info.b()}{host}{tag.white}:{tag.info.b()}{port}{tag.info} closed.{tag.close}")
+            
+
+        except socket.timeout:
+            print(f"\n\r{tag.error.b()}[-]{tag.error} Connection failed. Server connection timeout.{tag.close}")
         
-        json.dump(configuration, config_file, indent=4)
-        config_file.close()
-    
-    except FileNotFoundError as e:
-        print(f'{tag.error.b()}ERROR:{tag.error} configuration file not found:{tag.id}{e}{tag.close}')
-        exit(1)
-    
-    except json.JSONDecodeError as e:
-        print(f'{tag.error.b()}ERROR:{tag.error} json file decode failed:{tag.id}{e}{tag.close}')
+        except socket.error as e:
+            print(f"\n\r{tag.error.b()}[-]{tag.error} Connection failed. Server connection denied:\n\r{tag.close}{e}")
+        
+        except Exception as e:
+            print(f"\n\r{tag.error.b()}[-]{tag.error} Encountered runtime error:\n\r{tag.close}{e}")
+
+        finally:
+            os.remove(package)
+        
+    except Exception as e:
+        print(f'\r\n{tag.error.b()}ERROR:{tag.error} package failure: {tag.close}\n\r{e}')
         exit(1)
 
-    print(f"{tag.info.b()}Setup complete.{tag.info} Server configured with network {tag.id}{network}{tag.close}")
+if __name__ == "__main__":
+    # get server configurations
+    check_keychain()
+
+    configuration = fetch_config()
+
+    # Process and send container to server wrt config file
+    pack_and_ship(
+            configuration["serverIP"],
+            configuration["vanilla port"]
+    )
