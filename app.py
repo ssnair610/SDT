4ab54e665d507d66acff64a470032b6f86feeb66
Vivek Radhakrishnan <55615242+SassyQuatch47@users.noreply.github.com>
2022-12-28 16:07:10 +0530

    Updated app.py
    Regular update

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template,request,json
-
-import sys
+from flask import Flask ,redirect,render_template,request,json
+import subprocess
 import os
 
 __home__ = os.path.dirname(os.path.abspath(__file__))
@@ -19,8 +18,17 @@ def fileupload():
     with open(f'{__home__}/client-side/outbound/algorithm contract.json', 'w') as alg_contract_file:
         json.dump(data, alg_contract_file, indent=4)
 
-    os.system(f'python -u "{__home__}/client-side/talk.py"')
-    return render_template('summary.html')
+    subprocess.call(("python", "-u", f"{__home__}/client-side/talk.py"))
+
+    return redirect('/summary')
+
+@app.route('/summary')
+def summary():
+    summaryHTML = ''
+    with open(f'{__home__}/templates/summary.html', 'rt') as source_file:
+        summaryHTML = '\r\n'.join(source_file.readlines())
+    return summaryHTML
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
